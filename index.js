83accd8647458f77c446e939f6345c841ca1517a
adamhunt <adam-hunt@lambdastudents.com>
2021-02-22 17:30:23 -0600

    3/5

--- index.js
+++ index.js
@@ -28,9 +28,11 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  counter2 is local counter1 is global
+
   2. Which of the two uses a closure? How can you tell?
-  
+  counter2, function scope
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
@@ -62,8 +64,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+return Math.floor(Math.random() * Math.floor(3));
 }
 
 
@@ -81,8 +83,19 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(cb, num){
+  let finals = [];
+  let home = 0;
+  let away = 0;
+  for (let i = 0; i <= num; i++){
+  const currentHome = cb();
+  const currentAway = cb();
+  home += currentHome;
+  away += currentAway;
+  } 
+ finals.push({"Home": home , "Away": away})
+
+  return finals;
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,9 +103,15 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
-}
+  function getInningScore(cb) {
+    let scores = [];
+    let home = cb();
+    let away = cb();
+    scores.push({"Home": home, "Away": away})
+    return scores;  
+  }
+  
+  console.log(getInningScore(inning));
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
